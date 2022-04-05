351b6dea42bc70b55326040344372a7bf2836c5f
Charlie <trollol678534@gmail.com>
2022-04-05 11:04:51 -0700

    added user input and a recurring menu for the Sorts portion of calvin's code

--- Week_3/Sorts.java
+++ Week_3/Sorts.java
@@ -3,6 +3,7 @@ package Week_3;// package com.foo.sort;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Sorts {
     private ArrayList<Integer> data = new ArrayList<>();
@@ -34,67 +35,135 @@ public class Sorts {
 
 
     public static void main(String[] args) {
-        int sum=0, time=0, TIMES=12, SIZE=5000;
+        int sum = 0, time = 0, TIMES = 12, SIZE = 5000;
+        Scanner sc = new Scanner(System.in);
+        boolean tr = false;
+        while (!tr) {
+            System.out.println("Enter an algorithm(Bubble, Merge, Selection, Insertion, or enter 'all' to analyze all 4) to analyze, type 'exit' to leave: ");
+            String word = sc.nextLine();
+            //Bubble Week_2.Sort
+            if (word.equalsIgnoreCase("Bubble")) {
+                System.out.println("\n\n" + "Bubble Week_2.Sort" + "\n\n");
+                for (int i = 0; i < TIMES; i++) {
+                    BubbleSort s = new BubbleSort(SIZE);
+                    for (int j = 0; j < s.getData().size(); j++) {
+                        sum += s.getData().get(j);
+                    }
+                    time += s.getTimeElapsed();
+                }
+                System.out.println("Average random: " + sum / (TIMES * SIZE));
+                System.out.println("Total Nanoseconds: " + time);
+                System.out.println("Total Seconds: " + time / 1000000000.0);
+            } else if (word.equalsIgnoreCase("Selection")) {
+                //Selection Week_2.Sort
+                System.out.println("\n\n" + "Selection Week_2.Sort" + "\n\n");
+                sum = 0;
+                time = 0;
 
-        //Bubble Week_2.Sort
+                for (int i = 0; i < TIMES; i++) {
+                    SelectionSort s = new SelectionSort(SIZE);
+                    for (int j = 0; j < s.getData().size(); j++) {
+                        sum += s.getData().get(j);
+                    }
+                    time += s.getTimeElapsed();
+                }
+                System.out.println("Average random: " + sum / (TIMES * SIZE));
+                System.out.println("Total Nanoseconds: " + time);
+                System.out.println("Total Seconds: " + time / 1000000000.0);
+            } else if (word.equalsIgnoreCase("Insertion")) {
+                //Insertion Week_2.Sort
+                System.out.println("\n\n" + "Insertion Week_2.Sort" + "\n\n");
+                sum = 0;
+                time = 0;
 
-        System.out.println("\n\n" + "Bubble Week_2.Sort" + "\n\n");
-        for(int i=0; i< TIMES; i++) {
-            BubbleSort s = new BubbleSort(SIZE);
-            for(int j = 0; j<s.getData().size(); j++) {
-                sum += s.getData().get(j);
+                for (int i = 0; i < TIMES; i++) {
+                    InsertionSort s = new InsertionSort(SIZE);
+                    for (int j = 0; j < s.getData().size(); j++) {
+                        sum += s.getData().get(j);
+                    }
+                    time += s.getTimeElapsed();
+                }
+                System.out.println("Average random: " + sum / (TIMES * SIZE));
+                System.out.println("Total Nanoseconds: " + time);
+                System.out.println("Total Seconds: " + time / 1000000000.0);
+            } else if (word.equalsIgnoreCase("Merge")) {
+                //Merge Week_2.Sort
+                System.out.println("\n\n" + "Merge Week_2.Sort" + "\n\n");
+                sum = 0;
+                time = 0;
+
+                for (int i = 0; i < TIMES; i++) {
+                    MergeSort s = new MergeSort(SIZE);
+                    for (int j = 0; j < s.getData().size(); j++) {
+                        sum += s.getData().get(j);
+                    }
+                    time += s.getTimeElapsed();
+                }
+                System.out.println("Average random: " + sum / (TIMES * SIZE));
+                System.out.println("Total Nanoseconds: " + time);
+                System.out.println("Total Seconds: " + time / 1000000000.0);
+            } else if (word.equalsIgnoreCase("All")) {
+                System.out.println("\n\n" + "Bubble Week_2.Sort" + "\n\n");
+                for (int i = 0; i < TIMES; i++) {
+                    BubbleSort s = new BubbleSort(SIZE);
+                    for (int j = 0; j < s.getData().size(); j++) {
+                        sum += s.getData().get(j);
+                    }
+                    time += s.getTimeElapsed();
+                }
+                System.out.println("Average random: " + sum / (TIMES * SIZE));
+                System.out.println("Total Nanoseconds: " + time);
+                System.out.println("Total Seconds: " + time / 1000000000.0);
+
+                System.out.println("\n\n" + "Selection Week_2.Sort" + "\n\n");
+                sum = 0;
+                time = 0;
+
+                for (int i = 0; i < TIMES; i++) {
+                    SelectionSort s = new SelectionSort(SIZE);
+                    for (int j = 0; j < s.getData().size(); j++) {
+                        sum += s.getData().get(j);
+                    }
+                    time += s.getTimeElapsed();
+                }
+                System.out.println("Average random: " + sum / (TIMES * SIZE));
+                System.out.println("Total Nanoseconds: " + time);
+                System.out.println("Total Seconds: " + time / 1000000000.0);
+
+                System.out.println("\n\n" + "Insertion Week_2.Sort" + "\n\n");
+                sum = 0;
+                time = 0;
+
+                for (int i = 0; i < TIMES; i++) {
+                    InsertionSort s = new InsertionSort(SIZE);
+                    for (int j = 0; j < s.getData().size(); j++) {
+                        sum += s.getData().get(j);
+                    }
+                    time += s.getTimeElapsed();
+                }
+                System.out.println("Average random: " + sum / (TIMES * SIZE));
+                System.out.println("Total Nanoseconds: " + time);
+                System.out.println("Total Seconds: " + time / 1000000000.0);
+
+                System.out.println("\n\n" + "Merge Week_2.Sort" + "\n\n");
+                sum = 0;
+                time = 0;
+
+                for (int i = 0; i < TIMES; i++) {
+                    MergeSort s = new MergeSort(SIZE);
+                    for (int j = 0; j < s.getData().size(); j++) {
+                        sum += s.getData().get(j);
+                    }
+                    time += s.getTimeElapsed();
+                }
+                System.out.println("Average random: " + sum / (TIMES * SIZE));
+                System.out.println("Total Nanoseconds: " + time);
+                System.out.println("Total Seconds: " + time / 1000000000.0);
             }
-            time += s.getTimeElapsed();
-        }
-        System.out.println("Average random: " + sum / (TIMES*SIZE));
-        System.out.println("Total Nanoseconds: " + time );
-        System.out.println("Total Seconds: " + time /1000000000.0);
-
-        //Selection Week_2.Sort
-        System.out.println("\n\n" + "Selection Week_2.Sort" + "\n\n");
-        sum = 0; time=0;
-
-        for(int i=0; i< TIMES; i++) {
-            SelectionSort s = new SelectionSort(SIZE);
-            for(int j = 0; j<s.getData().size(); j++) {
-                sum += s.getData().get(j);
+            else if (word.equalsIgnoreCase("exit")){
+                break;
             }
-            time += s.getTimeElapsed();
         }
-        System.out.println("Average random: " + sum / (TIMES*SIZE));
-        System.out.println("Total Nanoseconds: " + time );
-        System.out.println("Total Seconds: " + time /1000000000.0);
-
-        //Insertion Week_2.Sort
-        System.out.println("\n\n" + "Insertion Week_2.Sort" + "\n\n");
-        sum = 0; time=0;
-
-        for(int i=0; i< TIMES; i++) {
-            InsertionSort s = new InsertionSort(SIZE);
-            for(int j = 0; j<s.getData().size(); j++) {
-                sum += s.getData().get(j);
-            }
-            time += s.getTimeElapsed();
-        }
-        System.out.println("Average random: " + sum / (TIMES*SIZE));
-        System.out.println("Total Nanoseconds: " + time );
-        System.out.println("Total Seconds: " + time /1000000000.0);
-
-    //Merge Week_2.Sort
-    System.out.println("\n\n" + "Merge Week_2.Sort" + "\n\n");
-    sum = 0; time=0;
-
-        for(int i=0; i< TIMES; i++) {
-        MergeSort s = new MergeSort(SIZE);
-        for(int j = 0; j<s.getData().size(); j++) {
-            sum += s.getData().get(j);
-        }
-        time += s.getTimeElapsed();
     }
-        System.out.println("Average random: " + sum / (TIMES*SIZE));
-        System.out.println("Total Nanoseconds: " + time );
-        System.out.println("Total Seconds: " + time /1000000000.0);
-}
-
 
 }
